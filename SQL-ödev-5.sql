f99006e9948da0014c3ef94fbab42b8b1afeead8
Sailorac <119450113+Sailorac@users.noreply.github.com>
2022-12-03 20:09:37 +0300

    Add files via upload

---
+++ SQL-ödev-5.sql
@@ -0,0 +1,21 @@
+--SELECT * FROM Film
+--where title like '%n'
+--order by length desc
+--limit 5 ;
+
+
+--SELECT * FROM film
+--where title like '%n'
+--order by length asc
+--offset 5 
+--limit 5
+
+
+Select * from customer
+where store_id = 1 
+order by last_name desc
+limit 4
+
+
+
+
